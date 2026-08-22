3e0e61502d154fc252069dddd7b27983cf7a861c
Sean <dradebosean@gmail.com>
2026-08-22 12:10:26 +0300

    fix: make Striving Observation the primary experience

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,10 @@ import { Sky, Environment, PerspectiveCamera, OrbitControls, Stats } from '@reac
 
 import type { DirectionalLight } from 'three'
 
-import { HideMouse, Keyboard } from './controls'
 import { Cameras } from './effects'
 import { BoundingBox, Ramp, Track, Vehicle, Goal, Train, Heightmap } from './models'
 import { angularVelocity, levelLayer, position, rotation, useStore } from './store'
-import { Checkpoint, Clock, Speed, Minimap, Intro, Help, Editor, LeaderBoard, Finished, PickColor } from './ui'
+import { Editor } from './ui'
 import { useToggle } from './useToggle'
 import { PitWall } from './racecraft/PitWall'
 import { RaceGhost } from './racecraft/RaceGhost'
@@ -20,19 +19,15 @@ layers.enable(levelLayer)
 
 export function App(): JSX.Element {
   const [light, setLight] = useState<DirectionalLight | null>(null)
-  const [actions, dpr, editor, shadows] = useStore((s) => [s.actions, s.dpr, s.editor, s.shadows])
-  const { onCheckpoint, onFinish, onStart } = actions
+  const [dpr, editor, shadows] = useStore((s) => [s.dpr, s.editor, s.shadows])
 
-  const ToggledCheckpoint = useToggle(Checkpoint, 'checkpoint')
   const ToggledDebug = useToggle(Debug, 'debug')
   const ToggledEditor = useToggle(Editor, 'editor')
-  const ToggledFinished = useToggle(Finished, 'finished')
-  const ToggledMap = useToggle(Minimap, 'map')
   const ToggledOrbitControls = useToggle(OrbitControls, 'editor')
   const ToggledStats = useToggle(Stats, 'stats')
 
   return (
-    <Intro>
+    <main className="striving-observation-app">
       <Canvas key={`${dpr}${shadows}`} dpr={[1, dpr]} shadows={shadows} camera={{ position: [0, 5, 15], fov: 50 }}>
         <fog attach="fog" args={['white', 0, 500]} />
         <Sky sunPosition={[100, 10, 100]} distance={1000} />
@@ -60,30 +55,17 @@ export function App(): JSX.Element {
             <Train />
             <Ramp args={[30, 6, 8]} position={[2, -1, 168.55]} rotation={[0, 0.49, Math.PI / 15]} />
             <Heightmap elementSize={0.5085} position={[327 - 66.5, -3.3, -473 + 213]} rotation={[-Math.PI / 2, 0, -Math.PI]} />
-            <Goal args={[0.001, 10, 18]} onCollideBegin={onStart} rotation={[0, 0.55, 0]} position={[-27, 1, 180]} />
-            <Goal args={[0.001, 10, 18]} onCollideBegin={onFinish} rotation={[0, -1.2, 0]} position={[-104, 1, -189]} />
-            <Goal args={[0.001, 10, 18]} onCollideBegin={onCheckpoint} rotation={[0, -0.5, 0]} position={[-50, 1, -5]} />
             <BoundingBox {...{ depth: 512, height: 100, position: [0, 40, 0], width: 512 }} />
           </ToggledDebug>
         </Physics>
         <Track />
         <RaceGhost />
         <Environment files="textures/dikhololo_night_1k.hdr" />
-        <ToggledMap />
         <ToggledOrbitControls />
       </Canvas>
-      <Clock />
       <ToggledEditor />
-      <ToggledFinished />
-      <Help />
-      <Speed />
       <ToggledStats />
-      <ToggledCheckpoint />
-      <LeaderBoard />
-      <PickColor />
-      <HideMouse />
-      <Keyboard />
       <PitWall />
-    </Intro>
+    </main>
   )
 }
