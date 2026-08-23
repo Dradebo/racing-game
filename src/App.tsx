4228b0ab11720d4f89b468bb99a780a790a1f9b0
Sean <dradebosean@gmail.com>
2026-08-24 00:21:59 +0300

    refactor: let native donor world own replay presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Layers } from 'three'
 import { Canvas } from '@react-three/fiber'
 import { Sky, Environment } from '@react-three/drei'
@@ -14,7 +15,9 @@ import { ReplayDirector } from './racecraft/ReplayDirector'
 import { TrophyShelf } from './racecraft/TrophyShelf'
 import { RaceRouteProvider } from './racecraft/RaceRouteProvider'
 import { NativeLapCapture } from './racecraft/NativeLapCapture'
+import { NativeReplayStage } from './racecraft/NativeReplayStage'
 import { RouteDiagnostic } from './racecraft/RouteDiagnostic'
+import { REPLAY_MODE_EVENT } from './racecraft/replayBridge'
 import './racecraft/appShell.css'
 
 const layers = new Layers()
@@ -22,7 +25,14 @@ layers.enable(levelLayer)
 
 export function App(): JSX.Element {
   const [dpr, shadows] = useStore((s) => [s.dpr, s.shadows])
+  const [replayActive, setReplayActive] = useState(false)
   const captureLap = new URLSearchParams(window.location.search).get('capture-lap') === '1'
+
+  useEffect(() => {
+    const handler = (event: Event) => setReplayActive(Boolean((event as CustomEvent<boolean>).detail))
+    window.addEventListener(REPLAY_MODE_EVENT, handler)
+    return () => window.removeEventListener(REPLAY_MODE_EVENT, handler)
+  }, [])
 
   if (captureLap) return <NativeLapCapture />
 
@@ -35,11 +45,15 @@ export function App(): JSX.Element {
           <ambientLight layers={layers} intensity={0.25} />
           <directionalLight layers={layers} position={[0, 50, 150]} intensity={1.1} castShadow />
           <Track />
-          <RaceRouteProvider>
-            <ObservationCamera />
-            <RaceScene />
-            <RaceGhost />
-          </RaceRouteProvider>
+          {replayActive ? (
+            <NativeReplayStage />
+          ) : (
+            <RaceRouteProvider>
+              <ObservationCamera />
+              <RaceScene />
+              <RaceGhost />
+            </RaceRouteProvider>
+          )}
           <Environment files="textures/dikhololo_night_1k.hdr" />
         </Canvas>
         <RacecraftHUD />
